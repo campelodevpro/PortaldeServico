8ded100c83f81eaf7547cbb6ee28c9a18760ef1b
Campelo Developer <campelodevpro@gmail.com>
2025-01-16 21:08:20 -0300

    first commit

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view ("comments.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'message' => 'required|string|max:255'
+        ],
+    [
+        'message.required'=> 'O campo mensagem é obrigatório',
+        'message.string'=> 'O campo mensagem deve ser um texto válido',
+        'message.max'=> 'O campo mensagem deve ter somente 255 caracteres',
+    ]);
+
+        $request->user()->comments()->create($validate);
+        return to_route('comments.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
